fc1563561f81c1eb747faf9483e7be1ced9b93cb
arror1784 <arror1784@gmail.com>
2020-07-07 18:04:16 +0900

    add daphne asgi

--- backend/djangoreactapi/routing.py
+++ backend/djangoreactapi/routing.py
@@ -2,7 +2,7 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 import post.routing
 
-
+ASGI_APPLICATION = "djangoreactapi.routing.application"
 application = ProtocolTypeRouter({
     # (http->django views is added by default)
 	    'websocket': AuthMiddlewareStack(
--- backend/djangoreactapi/settings.py
+++ backend/djangoreactapi/settings.py
@@ -139,7 +139,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = os.path.join(BASE_DIR,"static/")
 
 REST_FRAMEWORK = {
 	'DEFAULT_PERMISSION_CLASSES': [
